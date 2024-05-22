4efcd0c041b647572a7e0bbe31ed4475ae056cb9
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-05-22 21:01:48 +0530

    send some data to the udp server

--- udp_client.py
+++ udp_client.py
@@ -4,3 +4,5 @@ target_host = "127.0.0.1"
 target_port = 9987
 #creating the socket object
 client = sc.socket(sc.AF_INET,sc.SOCK_DGRAM)
+#now we send some data to the server
+client.sendto(b" hello i am a udp clinet tell me how are you",(target_host,target_port))
