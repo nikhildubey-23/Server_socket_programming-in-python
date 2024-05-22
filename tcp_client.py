c7a0e64c26c6cf2e30dd2430719a430d80de1f1e
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-05-22 20:51:42 +0530

    after sending the message server will respose back the to the client so in next step we print the response and decode so that we read and understand the response

--- tcp_client.py
+++ tcp_client.py
@@ -8,4 +8,8 @@ client = sc.socket()
 client.connect((target_host,target_port)) 
 #now we send some data to  the server
 client.send(b"i am anonymous clinet tell me your ip address\n")
-
+#after getting the message server will response back so for to receive to the message we have tp create method 
+response = client.recv(4049)
+#after getting the response we have to decode the response 
+print(response.decode())
+#after everything get over we will close the client server
