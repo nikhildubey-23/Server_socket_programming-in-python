79c3b008c712277bf30435873b1806644599d9d7
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-05-22 20:33:34 +0530

    creating the object of socket

--- tcp_client.py
+++ tcp_client.py
@@ -2,4 +2,6 @@ import socket as sc
 #step number 1 first create target host and target port for to connect with
 target_host = "127.0.0.1"
 target_port = 9956
+# now second step is to create a socket object
+client = sc.socket()
  
