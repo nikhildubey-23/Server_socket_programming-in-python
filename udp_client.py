32b7062f8b5ed607e621ed0c3a9f6e4644e1c2ec
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-05-23 09:48:13 +0530

    writing code for to receve data from the server and decode it

--- udp_client.py
+++ udp_client.py
@@ -6,3 +6,8 @@ target_port = 9987
 client = sc.socket(sc.AF_INET,sc.SOCK_DGRAM)
 #now we send some data to the server
 client.sendto(b" hello i am a udp clinet tell me how are you",(target_host,target_port))
+#receive some data from the server as a response 
+data , address = client.recvfrom(4906)
+#now we decode the data 
+print(data.decode())
+client.close()
