39a5a1e6a5deb6f3d30a64900e0d462b64854256
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-05-23 09:57:48 +0530

    now we bind the ip and port

--- tcp_server.py
+++ tcp_server.py
@@ -6,4 +6,5 @@ port = 9000
 def main():
     #now we create object for the socket 
     server = sc.socket(sc.AF_INET,sc.SOCK_STREAM)
-    
+    #now we bind the ip and port 
+    server.bind((ip,port))
