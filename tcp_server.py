212b173f5ce87ae14c547e71bfdf6698af29d47d
Nikhil Dubey <123150280+nikhildubey-23@users.noreply.github.com>
2024-05-23 09:56:25 +0530

    creating the object for the scoket module

--- tcp_server.py
+++ tcp_server.py
@@ -3,3 +3,7 @@ import threading  as th
 #now we set ip and port for the server
 ip = "127.0.0.1"
 port = 9000
+def main():
+    #now we create object for the socket 
+    server = sc.socket(sc.AF_INET,sc.SOCK_STREAM)
+    
